5e02b4e7e2830c1fe5a5f5a3e2946de215bfe698
dtl915 <3295874826@qq.com>
2023-01-03 12:50:03 +0800

    Version 2.1

--- Tetris.py
+++ Tetris.py
@@ -409,6 +409,9 @@ def main():
                     run=True
                     while run:
                         for event in pygame.event.get():
+                            if event.type==QUIT:
+                                pygame.quit()
+                                sys.exit()
                             if event.type==KEYDOWN:
                                 run=False    
 
